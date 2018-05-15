dd327bf9f12930312fadd6b623800b19b748a552
Pedro Chiericatti <chiericatti.dev@gmail.com>
2018-05-15 12:23:24 -0600

    library and photo update

--- funnel/funnel/Views+Controllers/PostSubmitAndReview/SubmitAndReviewViewController.swift
+++ funnel/funnel/Views+Controllers/PostSubmitAndReview/SubmitAndReviewViewController.swift
@@ -8,35 +8,94 @@
 
 import UIKit
 
-class SubmitAndReviewViewController: UIViewController {
+class SubmitAndReviewViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let picker = UIImagePickerController()
+
+    @IBOutlet weak var myImageView: UIButton!
+    @IBOutlet weak var myImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        showActionSheet()
+        picker.delegate = self
+        
     }
-
+    @IBAction func imageButtonTapped(_ sender: UIButton) {
+       showActionSheet()
+    }
+    
     func showActionSheet() {
         
         
         let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
-        let cancelAction = UIAlertAction(title: "Take Photo", style: .cancel, handler: nil)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         
         let takePhoto = UIAlertAction(title: "Take Photo", style: .default) { (_) in
             // show camera
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                self.picker.allowsEditing = false
+                self.picker.sourceType = UIImagePickerControllerSourceType.camera
+                self.picker.cameraCaptureMode = .photo
+                self.picker.modalPresentationStyle = .fullScreen
+                self.present(self.picker,animated: true,completion: nil)
+            } else {
+                noCamera()
+            }
+        }
+        
+        func noCamera(){
+            let alertVC = UIAlertController(
+                title: "No Camera",
+                message: "Sorry, this device has no camera",
+                preferredStyle: .alert)
+            let okAction = UIAlertAction(
+                title: "OK",
+                style:.default,
+                handler: nil)
+            alertVC.addAction(okAction)
+            present(
+                alertVC,
+                animated: true,
+                completion: nil)
         }
         
         let goToLibrary = UIAlertAction(title: "Photo Library", style: .default) { (_) in
             // take to library
+            
+            self.picker.allowsEditing = false
+            self.picker.sourceType = .photoLibrary
+            self.picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+            self.picker.modalPresentationStyle = .popover
+            self.present(self.picker, animated: true, completion: nil)
+//            self.picker.popoverPresentationController?.sourceRect = self
+            
         }
         
         actionSheet.addAction(cancelAction)
         actionSheet.addAction(takePhoto)
         actionSheet.addAction(goToLibrary)
         
+        present(actionSheet, animated: true, completion: nil)
     }
     
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        myImageView.contentMode = .scaleAspectFit //3
+        myImageView.setImage(chosenImage, for: .normal)
+        dismiss(animated:true, completion: nil) //5
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
