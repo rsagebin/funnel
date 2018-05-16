885545c9ca3ecfee743e8908db8b5356222ca6c1
Alec Osborne <alec.weston.osborne@gmail.com>
2018-05-16 10:52:51 -0600

    Minimal work on Following view

--- funnel/funnel/Views+Controllers/PersonalFeedViewController.swift
+++ funnel/funnel/Views+Controllers/PersonalFeedViewController.swift
@@ -10,22 +10,46 @@ import UIKit
 
 class PersonalFeedViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    
+    // MARK: - Outlets
+    @IBOutlet weak var followingTableView: UITableView!
+    
+//    let post
     
 
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    // MARK: - TableView Methods
+    // Sections
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // Rows
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "<#T##String#>", for: indexPath)
+//
+//        return cell
+//    }
+    
+    
     /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
