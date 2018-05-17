3b47702afc1f85507418ffb0b1b76948ba185bfe
rSage <rodrigo@sagebin.com>
2018-05-17 12:21:12 -0600

    Added some features to categoryVC and the Category Story file, along with CategoryController

--- funnel/funnel/Views+Controllers/CategoriesViewController.swift
+++ funnel/funnel/Views+Controllers/CategoriesViewController.swift
@@ -9,27 +9,47 @@
 import UIKit
 
 class CategoriesViewController: UIViewController {
-
+    @IBOutlet weak var category1Picker: UIPickerView!
+    @IBOutlet weak var category2Picker: UIPickerView!
+    @IBOutlet weak var category3Picker: UIPickerView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    let category = ["food", "health", "animals", "education", "transportation", "tech", "outdoors", "personal", "science", "lifestyle"]
+    
+    var selectedCategory: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // self.view.backgroundColor = UIColor(patternImage: )
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //backgroundGif.loadGif(asset: "inception")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func categorySearchButtonTapped(_ sender: Any) {
+        
+        // segue to feedVC listing cells of items within the specified category(ies)
     }
-    */
-
+    
 }
+
+extension CategoriesViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return category.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return category[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedCategory = category[row]
+        categoryLabel.text = selectedCategory
+    }
+}
+
+
