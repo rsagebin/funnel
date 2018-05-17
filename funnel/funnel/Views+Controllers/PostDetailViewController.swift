0e485cfd17e29926969874967c9258c39a7843f6
Pedro Chiericatti <chiericatti.dev@gmail.com>
2018-05-17 13:54:17 -0600

    pedro

--- funnel/funnel/Views+Controllers/PostDetailViewController.swift
+++ funnel/funnel/Views+Controllers/PostDetailViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class PostDetailViewController: UIViewController {
 
-    var post: Post?
+    var post: MockPost?
     
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
