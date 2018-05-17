f2dbbbf1ba98ff31e81dfd496e4fb445c1630a6f
Drew Carver <you@example.com>
2018-05-17 09:38:19 -0600

    Couple changes

--- funnel/funnel/Model+Controller/PostController.swift
+++ funnel/funnel/Model+Controller/PostController.swift
@@ -15,19 +15,23 @@ class PostController {
     
     static let shared = PostController()
     
-    // Possibly have two collections of posts - Feed and Following.
     // Possibly use NSPredicate in CKManager to specific time frame, etc. Or implement infinite scroll.
     
     // Use notification center to notifiy feed view/following view when posts have been loaded to refresh the tableview
     
-    // No thouchy
+
+    // No touchy - mock data
     var mockFeedPosts: [Post] = []
     
-    var posts = [Post]()
+    var feedPosts = [Post]()
+    var followingPosts = [Post]()
     
     
     func createPost(user: User, description: String, image: UIImage, category: String) {
-        let creatorReference = CKReference(recordID: user.ckRecord.recordID, action: .deleteSelf)
+        
+        // Ask about a more efficient way to get record ID without having to create a new CK record every time.
+        
+        let creatorReference = CKReference(recordID: user.ckRecordID ?? user.ckRecord.recordID, action: .deleteSelf)
         let post = Post(user: user, description: description, image: image, category: category, creatorRef: creatorReference)
         
         ckManager.save(records: [post.ckRecord], perRecordCompletion: nil) { (record, error) in
@@ -38,8 +42,8 @@ class PostController {
         }
     }
     
+    
     func fetchFeedPosts() {
-        
         let predicate = NSPredicate(value: true)
         
         ckManager.fetch(type: Post.typeKey, predicate: predicate) { (records, error) in
@@ -54,54 +58,43 @@ class PostController {
             }
             
             let recordsArray = records.compactMap( {Post(cloudKitRecord: $0) })
+            
+            self.feedPosts = recordsArray
         }
     }
     
-    func fetchUserPosts() {
+    func fetchFollowingPosts() {
         
-    }
-    
-    func setUpMockData() {
+        // FIXME: This predicate needs to be updated to only pull posts that the person is following
+        let predicate = NSPredicate(value: true)
         
-        var userAppleIDReference: CKReference?
-        
-        ckManager.getUserRecordID { (recordID, error) in
+        ckManager.fetch(type: Post.typeKey, predicate: predicate) { (records, error) in
             if let error = error {
-                print("Error getting user's Apple ID: \(error)")
+                print("Error fetching posts from CloudKit: \(error.localizedDescription)")
                 return
             }
             
-            guard let recordID = recordID else { return }
+            guard let records = records else {
+                print("Found nil for records fetched from CloudKit.")
+                return
+            }
             
-            userAppleIDReference = CKReference(recordID: recordID, action: .deleteSelf)
+            let recordsArray = records.compactMap( {Post(cloudKitRecord: $0) })
             
-            guard let reference = userAppleIDReference else { return }
-            
-            let user = User(username: "testing", name: "test2", appleUserRef: reference)
-            let post = Post(user: user, description: "Image description.", image: UIImage(named: "settings")!, category: "Test category", creatorRef: CKReference(record: user.ckRecord, action: .deleteSelf))
-            
-            let posts = [post]
-            
-            self.posts = posts
-            
-        
+            self.followingPosts = recordsArray
         }
+    }
         
 
-
-    }
     
     init() {
-        // No thouchy
         
+        // No touchy - mock data
         let user = User(username: "testing", name: "test2", appleUserRef: nil)
         let post = Post(user: user, description: "Image description.", image: UIImage(named: "settings")!, category: "Test category", creatorRef: CKReference(record: user.ckRecord, action: .deleteSelf))
         let post2 = Post(user: user, description: "Image description 2.", image: UIImage(named: "settings")!, category: "Test category", creatorRef: CKReference(record: user.ckRecord, action: .deleteSelf))
-
         self.mockFeedPosts = [post, post2]
-    
-        
-        setUpMockData()
+
     }
     
 }
