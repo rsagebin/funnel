e9df32f58e7de90d2ea694f6913f5500796e13d5
Drew Carver <you@example.com>
2018-05-18 15:44:09 -0600

    More CK work

--- funnel/funnel/Views+Controllers/FeedTableViewController.swift
+++ funnel/funnel/Views+Controllers/FeedTableViewController.swift
@@ -17,14 +17,17 @@ class FeedTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        PostController.shared.fetchFeedPosts()
+        
         NotificationCenter.default.addObserver(self, selector: #selector(reloadFeedView), name: NSNotification.Name(PostController.feedFetchCompletedNotificationName), object: nil)
     }
     
     @objc func reloadFeedView() {
         
+        
         DispatchQueue.main.async {
             
-            tableView.reloadData()
+            self.tableView.reloadData()
         }
     }
     
