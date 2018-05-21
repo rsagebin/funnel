91e16705d904a1f0217f68c055bad0968faaa5e3
Pedro Chiericatti <chiericatti.dev@gmail.com>
2018-05-21 12:04:20 -0600

    update on feed and detail controllers

--- funnel/funnel/Views+Controllers/FeedTableViewCell.swift
+++ funnel/funnel/Views+Controllers/FeedTableViewCell.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
+protocol CommentsDelegate {
+    func didTapComment()
+}
+
 class FeedTableViewCell: UITableViewCell {
 
     // MARK: - Properties
+    
+    var delegate: CommentsDelegate?
     
     var post: Post? {
         didSet {
@@ -25,10 +31,19 @@ class FeedTableViewCell: UITableViewCell {
     @IBOutlet weak var categoriesLabel: UILabel!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var exclamationButtonOutlet: UIButton!
-    @IBOutlet weak var notificationButtonOutlet: UIButton!
+    @IBOutlet weak var commentsButtonOutlet: UIButton!
+    
     @IBOutlet weak var branchButtonOutlet: UIButton!
     @IBOutlet weak var aprovedButtonOutlet: UIButton!
    
+    // MARK: - Actions
+    
+    @IBAction func commentsButtonTapped(_ sender: Any) {
+        print("Trying to show comments...")
+        delegate?.didTapComment()
+    }
+    
+    
     // MARK: - Other Functions
     
     func updateViews() {
