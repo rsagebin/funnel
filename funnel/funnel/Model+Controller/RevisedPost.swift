2a8713482d303b71b901a0c32ccb6231881ec623
Drew Carver <you@example.com>
2018-05-23 13:44:06 -0600

    More CK work

--- funnel/funnel/Model+Controller/RevisedPost.swift
+++ funnel/funnel/Model+Controller/RevisedPost.swift
@@ -19,6 +19,7 @@ class RevisedPost {
     private static let followersRefsKey = "followersRefs"
     private static let imageAsCKAssetKey = "imageAsCKAsset"
     private static let creatorRefKey = "creatorRef"
+    private static let postReferenceKey = "postReference"
     
 
     let description: String
@@ -35,6 +36,8 @@ class RevisedPost {
     var categoryAsString = ""
 
     let creatorRef: CKReference
+    
+    let postReference: CKReference
     
     var followersRefs: [CKReference]?
 
@@ -63,33 +66,40 @@ class RevisedPost {
         
         record.setValue(description, forKey: RevisedPost.descriptionKey)
         record.setValue(categoryAsString, forKey: RevisedPost.categoryAsStringKey)
+        record.setValue(creatorRef, forKey: RevisedPost.creatorRefKey)
+        record.setValue(postReference, forKey: RevisedPost.postReferenceKey)
 
         return record
     }
 
-    
-    // Create Failable initializer
-    
-//    init?(cloudKitRecord: CKRecord) {
-//        guard let description = cloudKitRecord[RevisedPost.descriptionKey] as? String,
-//            let imageAsCKAsset = cloudKitRecord[RevisedPost.imageAsCKAssetKey] as? CKAsset,
-//            let creatorRef = cloudKitRecord[RevisedPost.creatorRefKey] as? CKReference,
-//            let categoryAsString = cloudKitRecord[RevisedPost.categoryAsStringKey] as? String
-//            else { return nil }
-//
-//        if let category1Ref = cloudKitRecord[RevisedPost.category1RefKey] as? CKReference {
-//            self.category1Ref = category1Ref
-//        }
-//
-//        if let category2Ref = cloudKitRecord[RevisedPost.category2RefKey] as? CKReference {
-//            self.category2Ref = category2Ref
-//        }
-//
-//        if let category3Ref = cloudKitRecord[RevisedPost.category3RefKey] as? CKReference {
-//            self.category3Ref = category3Ref
-//        }
-//    }
-//
+    init?(cloudKitRecord: CKRecord) {
+        guard let description = cloudKitRecord[RevisedPost.descriptionKey] as? String,
+            let imageAsCKAsset = cloudKitRecord[RevisedPost.imageAsCKAssetKey] as? CKAsset,
+            let creatorRef = cloudKitRecord[RevisedPost.creatorRefKey] as? CKReference,
+            let categoryAsString = cloudKitRecord[RevisedPost.categoryAsStringKey] as? String,
+            let postReference = cloudKitRecord[RevisedPost.postReferenceKey] as? CKReference
+            else { return nil }
+
+        if let category1Ref = cloudKitRecord[RevisedPost.category1RefKey] as? CKReference {
+            self.category1Ref = category1Ref
+        }
+
+        if let category2Ref = cloudKitRecord[RevisedPost.category2RefKey] as? CKReference {
+            self.category2Ref = category2Ref
+        }
+
+        if let category3Ref = cloudKitRecord[RevisedPost.category3RefKey] as? CKReference {
+            self.category3Ref = category3Ref
+        }
+        
+        self.description = description
+        self.imageAsCKAsset = imageAsCKAsset
+        self.creatorRef = creatorRef
+        self.categoryAsString = categoryAsString
+        self.postReference = postReference
+        
+    }
+
     init(post: Post, description: String, category1Ref: CKReference?, category2Ref: CKReference?, category3Ref: CKReference?) {
         self.description = description
         self.creatorRef = post.creatorRef
@@ -97,6 +107,9 @@ class RevisedPost {
         self.category1Ref = category1Ref
         self.category2Ref = category2Ref
         self.category3Ref = category3Ref
+        
+        let postReference = CKReference(recordID: post.ckRecordID ?? post.ckRecord.recordID, action: .deleteSelf)
+        self.postReference = postReference
         
     }
 
