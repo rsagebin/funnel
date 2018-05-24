a983c383a1be646566ce32ea44ad6d5da54e97a0
Drew Carver <you@example.com>
2018-05-24 14:04:18 -0600

    Work on categories

--- funnel/funnel/Model+Controller/CategoryController.swift
+++ funnel/funnel/Model+Controller/CategoryController.swift
@@ -14,23 +14,56 @@ class CategoryController {
 
     static var shared = CategoryController()
     
-    let topCategories: [Category1] = [Category1(title: "food"),
-                                      Category1(title: "health"),
-                                      Category1(title: "animals"),
-                                      Category1(title: "education"),
-                                      Category1(title: "transportation"),
-                                      Category1(title: "tech"),
-                                      Category1(title: "outdoors"),
-                                      Category1(title: "personal"),
-                                      Category1(title: "science"),
-                                      Category1(title: "lifestyle"),
-                                      Category1(title: "unknown")
-                                    ]
+    var topCategories: [Category1] = []
     
     var category2Categories: [Category2] = []
     
     var category3Categories: [Category3] = []
     
+    
+    func loadTopLevelCategories(completion: @escaping (Bool) -> Void) {
+        let predicate = NSPredicate(value: true)
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        ckManager.fetch(type: Category1.typeKey, predicate: predicate, sortDescriptor: sortDescriptor) { (records, error) in
+            if let error = error {
+                print("Error fetching top level categories: \(error)")
+                completion(false)
+            }
+        
+            guard let categoriesArray = records?.compactMap({ Category1(cloudKitRecord: $0) }) else { return }
+            
+            self.topCategories = categoriesArray
+        
+            completion(true)
+        }
+        
+        
+    }
+    
+    // Only run this function once to put the categories into the cloud, if you change CloudKit containers
+    func saveTopLevelCategories() {
+        let topCategories: [Category1] = [Category1(title: "food"),
+                                          Category1(title: "health"),
+                                          Category1(title: "animals"),
+                                          Category1(title: "education"),
+                                          Category1(title: "transportation"),
+                                          Category1(title: "tech"),
+                                          Category1(title: "outdoors"),
+                                          Category1(title: "personal"),
+                                          Category1(title: "science"),
+                                          Category1(title: "lifestyle"),
+                                          Category1(title: "unknown")
+            ]
+        
+        ckManager.save(records: topCategories.compactMap( { $0.ckRecord } ), perRecordCompletion: nil) { (_, error) in
+            if let error = error {
+                print("Error saving top level records to CloudKit: \(error)")
+            }
+            return
+        }
+
+        
+    }
     
     func addCategory2(to category1: Category1, categoryName: String) {
         let category = Category2(title: categoryName, parent: category1)
@@ -56,35 +89,9 @@ class CategoryController {
         }
     }
     
-    func loadCategories() {
-        
-        let predicate = NSPredicate(value: true)
-        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
-        
-        ckManager.fetch(type: Category3.typeKey, predicate: predicate, sortDescriptor: sortDescriptor) { (records, error) in
-            if let error = error {
-                print("Error fetching level 3 categories: \(error)")
-                return
-            }
-            
-            let records = records?.compactMap({ Category3(cloudKitRecord: $0) }) ?? []
-            
-            self.category3Categories = records
-            
-        }
-        
-        ckManager.fetch(type: Category2.typeKey, predicate: predicate, sortDescriptor: sortDescriptor) { (records, error) in
-            if let error = error {
-                print("Error fetching level 2 categories: \(error)")
-                return
-            }
-            
-            let records = records?.compactMap({ Category2(cloudKitRecord: $0) }) ?? []
-            
-            self.category2Categories = records
-            
-        }
-        
+
+    func getCategory1Categories() {
+        //
     }
     
     func getCategory2Categories(category1: Category1) -> [Category2] {
