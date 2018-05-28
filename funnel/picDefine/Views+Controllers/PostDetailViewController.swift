4b10e9f8a7de7d7f88c8ee65e02cafbd90398072
Pedro Chiericatti <chiericatti.dev@gmail.com>
2018-05-28 09:41:33 -0600

    update

--- funnel/picDefine/Views+Controllers/PostDetailViewController.swift
+++ funnel/picDefine/Views+Controllers/PostDetailViewController.swift
@@ -134,7 +134,7 @@ class PostDetailViewController: UIViewController {
     @IBAction func branchPostButtonTapped(_ sender: Any) {
         print("button is working")
         let mySB = UIStoryboard(name: "CreateAndSuggest", bundle: .main)
-        let vc = mySB.instantiateViewController(withIdentifier: "PostAndSubmitSB") as! CreateAndSuggestViewController
+        let vc = mySB.instantiateViewController(withIdentifier: "CreateAndSuggestSB") as! CreateAndSuggestViewController
         vc.post = self.post
         navigationController?.pushViewController(vc, animated: true)
     }
